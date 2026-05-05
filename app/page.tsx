c61463c3b3affef5c921636db0e6bfec987c2951
Maria Wignall <boticaspa@gmail.com>
2026-05-05 12:57:35 -0500

    Reset scroll on page changes

--- app/page.tsx
+++ app/page.tsx
@@ -910,6 +910,12 @@ function AppShell() {
   }, [appSettings]);
 
   useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "auto" });
+    document.documentElement.scrollLeft = 0;
+    document.body.scrollLeft = 0;
+  }, [screen]);
+
+  useEffect(() => {
     let active = true;
 
     async function loadRealData() {
